11ddd782fe76aec1e0b543d9e2b85a0cd5ab0da5
Jacob Joel <joezy28@gmail.com>
2022-03-21 16:06:52 +0530

    task4

--- block-BJaabd/code/index.js
+++ block-BJaabd/code/index.js
@@ -11,6 +11,14 @@ Write a program that asks the user his/her age and check for the following condi
 👇
 */
 
+let age = Number(prompt(`Please enter your age .?`));
+
+if (age >=12 && age <=55){alert(`You can participate in the marathon`);}
+else if (age>=4 && age <= 11){alert(`You are too young to participate in the marathon`);}
+else if (age <4) {alert(`Hey Kiddo! Can You Walk ?`);}
+else if (age>55){alert(`Have fun Champ.`);}
+else {alert(`Please enter your age .`)};
+
 // Loops
 /*
 Given a positive integer `n`. Print the word (hello) in format of heeeello (letter 'e' must be repeated `n` times). Take input from prompt and print the result in alert.
